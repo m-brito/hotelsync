0318900239d61f0ecf26a8ffd49a1a0ffaf2503e
gabrmanh <gmanhani@yahoo.com>
2024-05-22 17:10:06 -0300

    feat: implemented update use case

---
+++ src/main/java/br/edu/ifsp/hotelsync/domain/usecases/product/update/UpdateProductUseCaseImpl.java
@@ -0,0 +1,24 @@
+package br.edu.ifsp.hotelsync.domain.usecases.product.update;
+
+import br.edu.ifsp.hotelsync.domain.entities.product.Product;
+import br.edu.ifsp.hotelsync.domain.persistence.dao.ProductDAO;
+
+import java.util.NoSuchElementException;
+
+public class UpdateProductUseCaseImpl implements UpdateProductUseCase {
+
+    private final ProductDAO repository;
+
+    public UpdateProductUseCaseImpl(ProductDAO repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void updateProduct(RequestModel requestModel) {
+        if(!repository.existsByKey(requestModel.id()))
+            throw new NoSuchElementException("Product of id " + requestModel.id() + " not found");
+
+        Product product = new Product(requestModel.id(), requestModel.description(), requestModel.price());
+        repository.update(product);
+    }
+}
