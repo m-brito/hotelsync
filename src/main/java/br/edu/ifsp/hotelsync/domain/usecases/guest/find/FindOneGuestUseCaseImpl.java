312ebe468963bfd5d1bef42611f81e21b03f5eee
unknown <britomauricio2003@gmail.com>
2024-05-22 23:59:35 -0300

    refactor: added error message when no guest is found by ID

--- src/main/java/br/edu/ifsp/hotelsync/domain/usecases/guest/find/FindOneGuestUseCaseImpl.java
+++ src/main/java/br/edu/ifsp/hotelsync/domain/usecases/guest/find/FindOneGuestUseCaseImpl.java
@@ -3,6 +3,8 @@ package br.edu.ifsp.hotelsync.domain.usecases.guest.find;
 import br.edu.ifsp.hotelsync.domain.entities.guest.Guest;
 import br.edu.ifsp.hotelsync.domain.persistence.dao.GuestDAO;
 
+import java.util.NoSuchElementException;
+
 public class FindOneGuestUseCaseImpl implements FindOneGuestUseCase{
     private final GuestDAO repository;
 
@@ -12,6 +14,8 @@ public class FindOneGuestUseCaseImpl implements FindOneGuestUseCase{
 
     @Override
     public Guest findOneById(RequestModel request) {
-        return repository.findOneByKey(request.id()).orElseThrow();
+        return repository.findOneByKey(request.id()).orElseThrow(
+                () -> new NoSuchElementException("Guest of id " + request.id() + " not found")
+        );
     }
 }
