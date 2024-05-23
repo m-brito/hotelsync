23dda03672a082573ca0230efe51858ab8ad58b3
unknown <britomauricio2003@gmail.com>
2024-05-22 23:20:39 -0300

    fix: correction to saving bankData when registering guests

--- src/main/java/br/edu/ifsp/hotelsync/domain/usecases/guest/create/CreateGuestUseCaseImpl.java
+++ src/main/java/br/edu/ifsp/hotelsync/domain/usecases/guest/create/CreateGuestUseCaseImpl.java
@@ -1,14 +1,16 @@
 package br.edu.ifsp.hotelsync.domain.usecases.guest.create;
 
 import br.edu.ifsp.hotelsync.domain.entities.guest.*;
+import br.edu.ifsp.hotelsync.domain.persistence.dao.BankDataDAO;
 import br.edu.ifsp.hotelsync.domain.persistence.dao.GuestDAO;
 
 public class CreateGuestUseCaseImpl implements CreateGuestUseCase {
-    private final GuestDAO repository;
+    private final GuestDAO guestRepository;
+    private final BankDataDAO bankDataRepository;
 
-
-    public CreateGuestUseCaseImpl(GuestDAO repository) {
-        this.repository = repository;
+    public CreateGuestUseCaseImpl(GuestDAO guestRepository, BankDataDAO bankDataRepository) {
+        this.guestRepository = guestRepository;
+        this.bankDataRepository = bankDataRepository;
     }
 
     @Override
@@ -19,6 +21,7 @@ public class CreateGuestUseCaseImpl implements CreateGuestUseCase {
         BankData bankData = new BankData(request.bank(), request.account(), request.agency(), request.cardDueDate(), request.cardOwner(), request.cardNumber());
         Guest guest = new Guest(request.name(), request.pronouns(), request.birthdate(), phone, cpf, address, bankData);
 
-        return repository.save(guest);
+        bankDataRepository.save(bankData);
+        return guestRepository.save(guest);
     }
 }
