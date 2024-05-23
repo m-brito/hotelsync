b46bbe3aaea9e7e7ebdc4f856e680a80b1e1c94a
unknown <britomauricio2003@gmail.com>
2024-05-22 23:23:08 -0300

    feat: update Guest use case

--- src/main/java/br/edu/ifsp/hotelsync/domain/usecases/guest/update/UpdateGuestUseCase.java
+++ src/main/java/br/edu/ifsp/hotelsync/domain/usecases/guest/update/UpdateGuestUseCase.java
@@ -1,2 +1,31 @@
-package br.edu.ifsp.hotelsync.domain.usecases.guest.update;public interface UpdateGuestUseCase {
+package br.edu.ifsp.hotelsync.domain.usecases.guest.update;
+
+import br.edu.ifsp.hotelsync.domain.usecases.product.update.UpdateProductUseCase;
+
+import java.time.LocalDate;
+
+public interface UpdateGuestUseCase {
+    record RequestModel(
+            Long id,
+            String name,
+            String pronouns,
+            LocalDate birthdate,
+            String phone,
+            String cpf,
+            String road,
+            String city,
+            String state,
+            String cep,
+            String district,
+            String complement,
+            boolean isActive,
+            String bank,
+            String account,
+            String agency,
+            String cardDueDate,
+            String cardOwner,
+            String cardNumber
+    ) {}
+
+    void updateGuest(RequestModel requestModel);
 }
