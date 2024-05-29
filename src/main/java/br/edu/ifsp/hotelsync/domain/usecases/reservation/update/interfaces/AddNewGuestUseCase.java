01a3d5e776a78ef822008a84afd4e71f342df931
lucas da silva cruz <lucas6jau@gmail.com>
2024-05-29 19:43:33 -0300

    fix: fixed interface lack of use of request

--- src/main/java/br/edu/ifsp/hotelsync/domain/usecases/reservation/update/interfaces/AddNewGuestUseCase.java
+++ src/main/java/br/edu/ifsp/hotelsync/domain/usecases/reservation/update/interfaces/AddNewGuestUseCase.java
@@ -5,5 +5,5 @@ import br.edu.ifsp.hotelsync.domain.persistence.dao.GuestDAO;
 public interface AddNewGuestUseCase {
     record RequestModel(Long idGuest, Long idReservation){}
 
-    void addNewGuest(Long idGuest, Long idReservation);
+    void addNewGuest(RequestModel request);
 }
