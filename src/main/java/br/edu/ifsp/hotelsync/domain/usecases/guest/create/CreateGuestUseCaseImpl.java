944fb1cd606f6cb2c178cc683a84d5c14e6f0f1a
lucas da silva cruz <lucas6jau@gmail.com>
2024-06-02 11:07:33 -0300

    refactor: removal of bank data in create guest use case

--- src/main/java/br/edu/ifsp/hotelsync/domain/usecases/guest/create/CreateGuestUseCaseImpl.java
+++ src/main/java/br/edu/ifsp/hotelsync/domain/usecases/guest/create/CreateGuestUseCaseImpl.java
@@ -6,11 +6,11 @@ import br.edu.ifsp.hotelsync.domain.persistence.dao.GuestDAO;
 
 public class CreateGuestUseCaseImpl implements CreateGuestUseCase {
     private final GuestDAO guestRepository;
-    private final BankDataDAO bankDataRepository;
 
-    public CreateGuestUseCaseImpl(GuestDAO guestRepository, BankDataDAO bankDataRepository) {
+
+    public CreateGuestUseCaseImpl(GuestDAO guestRepository) {
         this.guestRepository = guestRepository;
-        this.bankDataRepository = bankDataRepository;
+
     }
 
     @Override
@@ -18,10 +18,8 @@ public class CreateGuestUseCaseImpl implements CreateGuestUseCase {
         Phone phone = new Phone(request.phone());
         Cpf cpf = new Cpf(request.cpf());
         Address address = new Address(request.road(), request.city(), request.state(), request.cep(), request.district(), request.complement());
-        BankData bankData = new BankData(request.bank(), request.account(), request.agency(), request.cardDueDate(), request.cardOwner(), request.cardNumber());
-        Guest guest = new Guest(request.name(), request.pronouns(), request.birthdate(), phone, cpf, address, bankData);
+        Guest guest = new Guest(request.name(), request.pronouns(), request.birthdate(), phone, cpf, address);
 
-        bankDataRepository.save(bankData);
         return guestRepository.save(guest);
     }
 }
