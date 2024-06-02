0e29bc4d6f830523ec1a65b4ad967b92cf7039bc
lucas da silva cruz <lucas6jau@gmail.com>
2024-06-02 11:05:26 -0300

    refactor: improvement of Reservation's Owner validation

---
+++ src/main/java/br/edu/ifsp/hotelsync/domain/entities/guest/OwnerInputRequestValidator.java
@@ -0,0 +1,43 @@
+package br.edu.ifsp.hotelsync.domain.entities.guest;
+
+import br.edu.ifsp.hotelsync.domain.usecases.utils.Notification;
+import br.edu.ifsp.hotelsync.domain.usecases.utils.Validator;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class OwnerInputRequestValidator extends Validator<Guest> {
+    @Override
+    public Notification validate(Guest guest) {
+
+        Notification notification = new Notification();
+
+        if(guest == null) {
+            notification.addError("Guest is null");
+            return notification;
+        }
+        if(Validator.isNullOrEmpty(guest.getName()))
+            notification.addError("Name is null or empty");
+        if(guest.getName().length() > 20)
+            notification.addError("Name may not exceed 20 characters in size");
+        if(Validator.isNullOrEmpty(guest.getPronouns()))
+            notification.addError("Pronouns is null or empty");
+        if(guest.getBirthdate() == null)
+            notification.addError("Birthdate is null");
+        if(Period.between(guest.getBirthdate(), LocalDate.now()).getYears() < 18)
+            notification.addError(" Reservation Owners must not be under the age of 18");
+        if(Period.between(guest.getBirthdate(), LocalDate.now()).getYears() > 140)
+            notification.addError(" Reservation Owner age can't be higher than human lifespan");
+        if(guest.getCpf() == null)
+            notification.addError(" Reservation Owners must register CPF ");
+        if(guest.getCpf().getValue().length() != 11)
+            notification.addError("CPF must be 11 characters in length");
+        if(guest.getAddress() == null)
+            notification.addError(" Reservation Owners must register address");
+        if(guest.getPhone() == null)
+            notification.addError(" Reservation Owners must register phone");
+        if(guest.getPhone().getValue().length() > 14 || guest.getPhone().getValue().length() < 10)
+            notification.addError(" Reservation Owners must register a valid phone number");
+        return notification;
+    }
+}
