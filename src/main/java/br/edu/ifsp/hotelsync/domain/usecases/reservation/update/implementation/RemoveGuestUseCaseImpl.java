8d5b5651ee6c75261deed19417ad30d363004027
gabrmanh <gmanhani@yahoo.com>
2024-06-02 21:42:24 -0300

    feat: create RemoveGuestUseCase

--- src/main/java/br/edu/ifsp/hotelsync/domain/usecases/reservation/update/implementation/RemoveGuestUseCaseImpl.java
+++ src/main/java/br/edu/ifsp/hotelsync/domain/usecases/reservation/update/implementation/RemoveGuestUseCaseImpl.java
@@ -4,23 +4,23 @@ import br.edu.ifsp.hotelsync.domain.entities.guest.Guest;
 import br.edu.ifsp.hotelsync.domain.entities.reservation.Reservation;
 import br.edu.ifsp.hotelsync.domain.persistence.dao.GuestDao;
 import br.edu.ifsp.hotelsync.domain.persistence.dao.ReservationDao;
-import br.edu.ifsp.hotelsync.domain.usecases.reservation.update.interfaces.AddNewGuestUseCase;
+import br.edu.ifsp.hotelsync.domain.usecases.reservation.update.interfaces.RemoveGuestUseCase;
 
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
-public class AddNewGuestUseCaseImpl implements AddNewGuestUseCase {
+public class RemoveGuestUseCaseImpl implements RemoveGuestUseCase {
 
     private final GuestDao guestRepository;
     private final ReservationDao reservationRepository;
 
-    public AddNewGuestUseCaseImpl(GuestDao guestRepository, ReservationDao reservationRepository) {
+    public RemoveGuestUseCaseImpl(GuestDao guestRepository, ReservationDao reservationRepository) {
         this.guestRepository = guestRepository;
         this.reservationRepository = reservationRepository;
     }
 
     @Override
-    public void addNewGuest(RequestModel request) {
+    public void removeGuest(RequestModel request) {
         Optional<Guest> guest = guestRepository.findOneByKey(request.idGuest());
         if(guest.isEmpty())
             throw new NoSuchElementException("Guest of id " + request.idReservation() + " not found");
@@ -29,9 +29,9 @@ public class AddNewGuestUseCaseImpl implements AddNewGuestUseCase {
         if(reservation.isEmpty())
             throw new NoSuchElementException("Reservation of id " + request.idReservation() + " not found");
 
-        if(reservation.get().getGuests().contains(guest.get()))
-            throw new NoSuchElementException("Guest " + request.idReservation() + " Already exists in this reservation");
+        if(!reservation.get().getGuests().contains(guest.get()))
+            throw new NoSuchElementException("Guest " + request.idReservation() + " not present in this reservation");
 
-        reservation.get().addGuest(guest.get());
+        reservation.get().removeGuest(guest.get());
     }
 }
--- src/main/java/br/edu/ifsp/hotelsync/domain/usecases/reservation/update/interfaces/AddGuestUseCase.java
+++ src/main/java/br/edu/ifsp/hotelsync/domain/usecases/reservation/update/interfaces/AddGuestUseCase.java
@@ -1,7 +1,7 @@
 package br.edu.ifsp.hotelsync.domain.usecases.reservation.update.interfaces;
 
-public interface AddNewGuestUseCase {
+public interface AddGuestUseCase {
     record RequestModel(Long idGuest, Long idReservation){}
 
-    void addNewGuest(RequestModel request);
+    void addGuest(RequestModel request);
 }
