59e8a3b104c393b79367c3a27d32e84d7656b91f
vmacena <macena.v@aluno.ifsp.edu.br>
2024-06-11 00:44:54 -0300

    feat: add icons and update links

--- src/main/java/br/edu/ifsp/hotelsync/application/view/Application.java
+++ src/main/java/br/edu/ifsp/hotelsync/application/view/Application.java
@@ -1,6 +1,7 @@
 package br.edu.ifsp.hotelsync.application.view;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -8,18 +9,32 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
+    private static Scene scene;
+    private static Object controller;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);        stage.setTitle("HotelSync");
+        scene = new Scene(loadFXML("home"));
         stage.getIcons().add(new Image("file:src/main/resources/br/edu/ifsp/hotelsync/application/view/icon.png"));
         stage.setMinWidth(1068);
         stage.setMinHeight(666);
-
-
-
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource(fxml + ".fxml"));
+        Parent parent = fxmlLoader.load();
+        controller = fxmlLoader.getController();
+        return parent;
+    }
+
+    public static Object getController() {
+        return controller;
     }
 
     public static void main(String[] args) {
