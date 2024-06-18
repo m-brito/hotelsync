8e55b2b1536c92384d2fabe3055cf0b8d1559659
vmacena <macena.v@aluno.ifsp.edu.br>
2024-06-18 17:52:08 -0300

    refactor: deleting unused modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,12 +7,11 @@ module br.edu.ifsp.hotelsync {
     opens br.edu.ifsp.hotelsync.application.view to javafx.fxml;
 
     exports br.edu.ifsp.hotelsync.application.view;
-    exports br.edu.ifsp.hotelsync.application.controller.addControllers;
-    opens br.edu.ifsp.hotelsync.application.controller.addControllers to javafx.fxml;
+    exports br.edu.ifsp.hotelsync.application.controller.createControllers;
+    opens br.edu.ifsp.hotelsync.application.controller.createControllers to javafx.fxml;
     exports br.edu.ifsp.hotelsync.application.controller.entitiesControllers;
     opens br.edu.ifsp.hotelsync.application.controller.entitiesControllers to javafx.fxml;
     exports br.edu.ifsp.hotelsync.application.controller;
     opens br.edu.ifsp.hotelsync.application.controller to javafx.fxml;
-    exports br.edu.ifsp.hotelsync.application.view.entitiesViews;
-    opens br.edu.ifsp.hotelsync.application.view.entitiesViews to javafx.fxml;
+
 }
