10f1001ec3769f04755181b9981fe99207783222
m-brito <britomauricio2003@gmail.com>
2024-06-19 00:59:48 -0300

    fix: correction of payment on reserves

--- src/main/java/br/edu/ifsp/hotelsync/domain/usecases/reservation/update/interfaces/CheckOutUseCase.java
+++ src/main/java/br/edu/ifsp/hotelsync/domain/usecases/reservation/update/interfaces/CheckOutUseCase.java
@@ -1,7 +1,9 @@
 package br.edu.ifsp.hotelsync.domain.usecases.reservation.update.interfaces;
 
+import br.edu.ifsp.hotelsync.domain.entities.reservation.Payment;
+
 public interface CheckOutUseCase {
-    record RequestModel(Long id, String paymentMethod){}
+    record RequestModel(Long id, Payment paymentMethod){}
 
     void doCheckOut(RequestModel request);
 }
