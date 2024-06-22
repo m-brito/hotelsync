1c8ab73b13331be0f3901ed4e1e57d15ad511af9
m-brito <britomauricio2003@gmail.com>
2024-06-22 18:04:10 -0300

    refactor: deletion of unnecessary alerts

--- src/main/java/br/edu/ifsp/hotelsync/application/util/alertHandler/IAlertHandler.java
+++
@@ -1,5 +0,0 @@
-package br.edu.ifsp.hotelsync.application.util.alertHandler;
-
-import javafx.scene.control.Alert;
-
-public interface IAlertHandler { Alert createExitConfirmationAlert(); }
--- src/main/java/br/edu/ifsp/hotelsync/application/util/exitHandler/IExitHandler.java
+++
@@ -1,5 +0,0 @@
-package br.edu.ifsp.hotelsync.application.util.exitHandler;
-
-import javafx.event.ActionEvent;
-
-public interface IExitHandler { void handleExit(ActionEvent event); }
