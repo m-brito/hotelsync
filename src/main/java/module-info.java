2db5da49a594532f30531804eee05f1a6b0d6081
m-brito <britomauricio2003@gmail.com>
2024-06-23 01:15:53 -0300

    fix: module corrections

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,10 +8,14 @@ module br.edu.ifsp.hotelsync {
     opens br.edu.ifsp.hotelsync.application.view to javafx.fxml;
 
     exports br.edu.ifsp.hotelsync.application.view;
-    exports br.edu.ifsp.hotelsync.application.controller.managementControllers;
-    opens br.edu.ifsp.hotelsync.application.controller.managementControllers to javafx.fxml;
     exports br.edu.ifsp.hotelsync.application.controller;
+    exports br.edu.ifsp.hotelsync.application.controller.managementControllers;
+
+    opens br.edu.ifsp.hotelsync.application.controller.managementControllers to javafx.fxml;
     opens br.edu.ifsp.hotelsync.application.controller to javafx.fxml;
+
     opens br.edu.ifsp.hotelsync.domain.entities.product to javafx.base;
     opens br.edu.ifsp.hotelsync.domain.entities.guest to javafx.base;
+    opens br.edu.ifsp.hotelsync.domain.entities.room to javafx.base;
+    opens br.edu.ifsp.hotelsync.domain.entities.reservation to javafx.base;
 }
