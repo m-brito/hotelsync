b723f724bcb2e77260cbecaadd9883588df9e410
vmacena <macena.v@aluno.ifsp.edu.br>
2024-06-23 20:33:14 -0300

    feat: test

--- src/main/java/br/edu/ifsp/hotelsync/application/controller/managementControllers/ReportManagementController.java
+++ src/main/java/br/edu/ifsp/hotelsync/application/controller/managementControllers/ReportManagementController.java
@@ -1,14 +1,35 @@
 package br.edu.ifsp.hotelsync.application.controller.managementControllers;
 
+import br.edu.ifsp.hotelsync.application.repository.sqlite.dao.SqliteReservationDao;
+import br.edu.ifsp.hotelsync.application.repository.sqlite.dao.SqliteRoomDao;
 import br.edu.ifsp.hotelsync.application.util.ExitHandler;
 import br.edu.ifsp.hotelsync.application.util.NavigationHandler;
+import br.edu.ifsp.hotelsync.domain.entities.product.Product;
 import br.edu.ifsp.hotelsync.domain.entities.report.exporter.Type;
+import br.edu.ifsp.hotelsync.domain.entities.report.formatter.Formatter;
+import br.edu.ifsp.hotelsync.domain.entities.report.formatter.SimpleTextFormatter;
+import br.edu.ifsp.hotelsync.domain.entities.report.records.CheckInReport;
+import br.edu.ifsp.hotelsync.domain.entities.report.records.DailyOccupationReport;
+import br.edu.ifsp.hotelsync.domain.entities.report.records.Exportable;
+import br.edu.ifsp.hotelsync.domain.persistence.dao.ReservationDao;
+import br.edu.ifsp.hotelsync.domain.persistence.dao.RoomDao;
+import br.edu.ifsp.hotelsync.domain.usecases.reports.create.CreateDailyOccupationReportUseCase;
+import br.edu.ifsp.hotelsync.domain.usecases.reports.create.CreateReportUseCase;
+import br.edu.ifsp.hotelsync.domain.usecases.reports.export.PdfExportUseCaseImpl;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Map;
+
+import static br.edu.ifsp.hotelsync.application.main.Main.createCheckInReport;
 
 public class ReportManagementController {
     @FXML
@@ -39,10 +60,10 @@ public class ReportManagementController {
     private Button dailyOcuppationButton;
 
     @FXML
-    private TableColumn<?, ?> dateColumn;
+    private TableColumn<Exportable, String> dateColumn;
 
     @FXML
-    private DatePicker endDaePicker;
+    private DatePicker endDatePicker;
 
     @FXML
     private Button exportBtn;
@@ -60,14 +81,18 @@ public class ReportManagementController {
     private DatePicker startDatePicker;
 
     @FXML
-    private TableColumn<?, ?> valueColumn;
+    private TableColumn<Exportable, String> valueColumn;
 
     @FXML
-    private TableView<?> tableFinancial;
+    private TableView<Exportable> tableFinancial;
+
+    private ObservableList<Exportable> tableData;
 
     @FXML
     public void initialize() {
         exportTypeCombo.getItems().addAll(Type.values());
+        bindTableViewToItemsList();
+        bindColumnsToValuesSources();
     }
 
     private final NavigationHandler navHandler =
@@ -99,5 +124,32 @@ public class ReportManagementController {
     @FXML
     void handleRoomPage(ActionEvent event) throws IOException {
         navHandler.navigateToRoomManagementPage();
+    }
+
+    private void bindTableViewToItemsList() {
+        tableData = FXCollections.observableArrayList();
+        tableFinancial.setItems(tableData);
+    }
+
+    private void bindColumnsToValuesSources() {
+        dateColumn.setCellValueFactory(cell -> {
+            Object cellValue = cell.getValue();
+            if (cellValue instanceof Exportable) {
+                Exportable exportable = (Exportable) cellValue;
+                String key = exportable.getReport().keySet().isEmpty() ? "" : exportable.getReport().keySet().iterator().next().toString();
+                return new SimpleStringProperty(key);
+            } else {
+                // Handle the case when cellValue is not an Exportable
+                return new SimpleStringProperty(cellValue.toString());
+            }
+        });
+    }
+
+    public void handleCheckInReport(ActionEvent actionEvent) {
+        Exportable report = createCheckInReport.createReport(new CreateReportUseCase.RequestModel(
+                startDatePicker.getValue(),
+                endDatePicker.getValue()));
+        tableData.clear();
+        tableData.addAll(report.getReport().values());
     }
 }
