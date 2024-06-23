4afc35646e37b917835b088eb4c306c7027c8526
m-brito <britomauricio2003@gmail.com>
2024-06-23 04:45:55 -0300

    feat: add function to search for available rooms

--- src/main/java/br/edu/ifsp/hotelsync/domain/persistence/dao/RoomDao.java
+++ src/main/java/br/edu/ifsp/hotelsync/domain/persistence/dao/RoomDao.java
@@ -3,8 +3,12 @@ package br.edu.ifsp.hotelsync.domain.persistence.dao;
 import br.edu.ifsp.hotelsync.domain.entities.room.Room;
 import br.edu.ifsp.hotelsync.domain.persistence.util.Dao;
 
+import java.util.Map;
+
 public interface RoomDao extends Dao<Long, Room> {
 
     int getTotalRooms();
+
+    Map<Long, Room> findAllAvailable();
 
 }
