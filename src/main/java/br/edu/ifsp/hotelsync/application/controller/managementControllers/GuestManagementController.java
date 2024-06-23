d8c66f16ce1a7dccad944a4351196121a77d5b9f
m-brito <britomauricio2003@gmail.com>
2024-06-22 22:48:00 -0300

    feat: add bind table and popular table logic

--- src/main/java/br/edu/ifsp/hotelsync/application/controller/managementControllers/GuestManagementController.java
+++ src/main/java/br/edu/ifsp/hotelsync/application/controller/managementControllers/GuestManagementController.java
@@ -1,20 +1,25 @@
 package br.edu.ifsp.hotelsync.application.controller.managementControllers;
 
 import br.edu.ifsp.hotelsync.application.controller.GuestController;
-import br.edu.ifsp.hotelsync.application.controller.tableControllers.GuestTableController;
 import br.edu.ifsp.hotelsync.application.util.ExitHandler;
 import br.edu.ifsp.hotelsync.application.util.NavigationHandler;
 import br.edu.ifsp.hotelsync.application.util.UIMode;
 import br.edu.ifsp.hotelsync.application.view.Home;
 import br.edu.ifsp.hotelsync.domain.entities.guest.Guest;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Map;
+
+import static br.edu.ifsp.hotelsync.application.main.Main.findAllGuestUseCase;
 
 public class GuestManagementController {
     @FXML
@@ -83,6 +88,8 @@ public class GuestManagementController {
     @FXML
     private Button updateGuestButton;
 
+    private ObservableList<Guest> tableData;
+
     private final NavigationHandler navHandler =
             new NavigationHandler();
 
@@ -92,20 +99,60 @@ public class GuestManagementController {
 
     @FXML
     public void initialize() {
-        GuestTableController guestTableHandler = new GuestTableController(
-                nameColumn,
-                pronounsColumn,
-                birthdateColumn,
-                phoneColumn,
-                cpfColumn,
-                roadColumn,
-                cityColumn,
-                stateColumn,
-                cepColumn,
-                districtColumn,
-                complementColumn,
-                tableGuest);
-        guestTableHandler.populateTable();
+        bindTableViewToItemsList();
+        bindColumnsToValuesSources();
+        populateTable();
+    }
+
+    private void bindTableViewToItemsList() {
+        tableData = FXCollections.observableArrayList();
+        tableGuest.setItems(tableData);
+    }
+
+    private void bindColumnsToValuesSources() {
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        pronounsColumn.setCellValueFactory(new PropertyValueFactory<>("pronouns"));
+        birthdateColumn.setCellValueFactory(new PropertyValueFactory<>("birthdate"));
+        phoneColumn.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        cpfColumn.setCellValueFactory(new PropertyValueFactory<>("cpf"));
+        roadColumn.setCellValueFactory(cell ->
+                new SimpleStringProperty(cell.getValue().getAddress() != null
+                        ? cell.getValue().getAddress().getRoad() : null));
+        cityColumn.setCellValueFactory(cell ->
+                new SimpleStringProperty(cell.getValue().getAddress() != null
+                        ? cell.getValue().getAddress().getCity() : null));
+        stateColumn.setCellValueFactory(cell ->
+                new SimpleStringProperty(cell.getValue().getAddress() != null
+                        ? cell.getValue().getAddress().getState().getName() : null));
+        cepColumn.setCellValueFactory(cell ->
+                new SimpleStringProperty(cell.getValue().getAddress() != null
+                        ? cell.getValue().getAddress().cep() : null));
+        districtColumn.setCellValueFactory(cell ->
+                new SimpleStringProperty(cell.getValue().getAddress() != null
+                        ? cell.getValue().getAddress().getDistrict() : null));
+        complementColumn.setCellValueFactory(cell ->
+                new SimpleStringProperty(cell.getValue().getAddress() != null
+                        ? cell.getValue().getAddress().getComplement() : null));
+    }
+
+    public void populateTable() {
+        Map<Long, Guest> guests = findAllGuestUseCase.findAll();
+        ObservableList<Guest> guestList = FXCollections.observableArrayList(guests.values());
+        tableGuest.setItems(guestList);
+    }
+
+    private void showGuestInMode(UIMode mode) throws IOException {
+        Guest selectedItem = tableGuest.getSelectionModel().getSelectedItem();
+        if (selectedItem != null) {
+            navHandler.handleCreateGuest();
+            GuestController controller = (GuestController) Home.getController();
+            controller.setEntity(selectedItem, mode);
+        }
+    }
+
+    @FXML
+    public void handleUpdateGuest(ActionEvent actionEvent) throws IOException {
+        showGuestInMode(UIMode.UPDATE);
     }
 
     @FXML
@@ -137,22 +184,9 @@ public class GuestManagementController {
     void handleRoomPage(ActionEvent event) throws IOException {
         navHandler.navigateToRoomPage();
     }
-    
+
     @FXML
     public void handleCreateGuest(ActionEvent actionEvent) throws IOException {
         navHandler.handleCreateGuest();
-    }
-
-    public void handleUpdateGuest(ActionEvent actionEvent) throws IOException {
-        showGuestInMode(UIMode.UPDATE);
-    }
-
-    private void showGuestInMode(UIMode mode) throws IOException {
-        Guest selectedItem = tableGuest.getSelectionModel().getSelectedItem();
-        if (selectedItem != null) {
-            navHandler.handleCreateGuest();
-            GuestController controller = (GuestController) Home.getController();
-            controller.setEntity(selectedItem, mode);
-        }
     }
 }
