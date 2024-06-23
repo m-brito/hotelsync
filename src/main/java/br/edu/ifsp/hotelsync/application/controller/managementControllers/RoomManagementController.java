d5697022d9b014b0914e256e1b0c493b871f3c32
m-brito <britomauricio2003@gmail.com>
2024-06-23 01:22:12 -0300

    feat: implements room management controller

--- src/main/java/br/edu/ifsp/hotelsync/application/controller/managementControllers/RoomManagementController.java
+++ src/main/java/br/edu/ifsp/hotelsync/application/controller/managementControllers/RoomManagementController.java
@@ -1,16 +1,27 @@
 package br.edu.ifsp.hotelsync.application.controller.managementControllers;
 
+import br.edu.ifsp.hotelsync.application.controller.RoomController;
 import br.edu.ifsp.hotelsync.application.util.ExitHandler;
 import br.edu.ifsp.hotelsync.application.util.NavigationHandler;
+import br.edu.ifsp.hotelsync.application.util.UIMode;
+import br.edu.ifsp.hotelsync.application.view.Home;
 import br.edu.ifsp.hotelsync.domain.entities.room.Room;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 
 import java.io.IOException;
+import java.util.Map;
+
+import static br.edu.ifsp.hotelsync.application.main.Main.findAllRoomUseCase;
 
 public class RoomManagementController {
     @FXML
@@ -44,21 +55,76 @@ public class RoomManagementController {
     private TableView<Room> tableRoom;
 
     @FXML
+    private TableColumn<Room, Double> numberColumn;
+
+    @FXML
+    private TableColumn<Room, Double> numberOfBedsColumn;
+
+    @FXML
+    private TableColumn<Room, Double> typeOfBedColumn;
+
+    @FXML
+    private TableColumn<Room, String> categoryColumn;
+
+    @FXML
+    private TableColumn<Room, String> descriptionColumn;
+
+    @FXML
+    private TableColumn<Room, String> statusColumn;
+
+    @FXML
+    private TableColumn<Room, Double> areaColumn;
+
+    @FXML
     private Button updateRoomButton;
+
+    private ObservableList<Room> tableData;
 
     @FXML
     public void initialize() {
+        bindTableViewToItemsList();
+        bindColumnsToValuesSources();
+        populateTable();
     }
 
-    private final ExitHandler exitHandler =
-            new ExitHandler();
+    private void bindTableViewToItemsList() {
+        tableData = FXCollections.observableArrayList();
+        tableRoom.setItems(tableData);
+    }
+
+    private void bindColumnsToValuesSources() {
+        numberColumn.setCellValueFactory(new PropertyValueFactory<>("number"));
+        numberOfBedsColumn.setCellValueFactory(new PropertyValueFactory<>("numberOfBeds"));
+        typeOfBedColumn.setCellValueFactory(new PropertyValueFactory<>("typeOfBed"));
+        categoryColumn.setCellValueFactory(cell ->
+                new SimpleStringProperty(cell.getValue().getRoomCategory().getDescription()));
+        descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+        statusColumn.setCellValueFactory(cell ->
+                new SimpleStringProperty(cell.getValue().getRoomStatus().toString()));
+        areaColumn.setCellValueFactory(new PropertyValueFactory<>("area"));
+    }
+
+    private void showProductInMode(UIMode mode) throws IOException {
+        Room selectedItem = tableRoom.getSelectionModel().getSelectedItem();
+        if (selectedItem != null) {
+            navHandler.navigateToRoomPage();
+            RoomController controller = (RoomController) Home.getController();
+            controller.setEntity(selectedItem, mode);
+        }
+    }
+
+    public void populateTable() {
+        Map<Long, Room> rooms = findAllRoomUseCase.findAll();
+        tableData.clear();
+        tableData.addAll(rooms.values());
+    }
 
     private final NavigationHandler navHandler =
             new NavigationHandler();
 
     @FXML
     void handleExit(ActionEvent event) {
-        exitHandler.handleExit(event);
+        new ExitHandler().handleExit(event);
     }
 
     @FXML
@@ -91,6 +157,7 @@ public class RoomManagementController {
         navHandler.navigateToRoomPage();
     }
 
-    public void handleUpdateRoom(ActionEvent actionEvent) {
+    public void handleUpdateRoom(ActionEvent actionEvent) throws IOException {
+        showProductInMode(UIMode.UPDATE);
     }
 }
