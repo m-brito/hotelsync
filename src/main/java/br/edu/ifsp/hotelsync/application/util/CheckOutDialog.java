7bacf9f4240f899f7b7094f622815359585d0f9d
vmacena <macena.v@aluno.ifsp.edu.br>
2024-06-24 20:17:53 -0300

    feat: checkOutDialog add total and change input

--- src/main/java/br/edu/ifsp/hotelsync/application/util/CheckOutDialog.java
+++ src/main/java/br/edu/ifsp/hotelsync/application/util/CheckOutDialog.java
@@ -2,30 +2,41 @@ package br.edu.ifsp.hotelsync.application.util;
 
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.VBox;
 import javafx.util.Pair;
 
 import java.time.LocalDate;
 
 public class CheckOutDialog extends Dialog<Pair<String, String>> {
-
-    public CheckOutDialog() {
-        setTitle("Check-out Dialog");
-        setHeaderText("Select a payment method and finalize the check-out.");
+    public CheckOutDialog(double totalReservation) {
+        setTitle("Check-out");
+        setHeaderText("Select a payment method and finalize the check-out:");
 
         ButtonType finalizeButtonType = new ButtonType("Finalize", ButtonBar.ButtonData.OK_DONE);
         getDialogPane().getButtonTypes().addAll(finalizeButtonType, ButtonType.CANCEL);
 
-        ComboBox<String> comboBox = new ComboBox<>();
-        comboBox.getItems().addAll("Cash", "Credit Card", "Debit");
-        comboBox.setMaxWidth(50);
+        RadioButton cashButton = new RadioButton("Cash");
+        RadioButton creditCardButton = new RadioButton("Credit Card");
+        RadioButton debitButton = new RadioButton("Debit");
 
-        getDialogPane().setContent(comboBox);
+        ToggleGroup group = new ToggleGroup();
+        cashButton.setToggleGroup(group);
+        creditCardButton.setToggleGroup(group);
+        debitButton.setToggleGroup(group);
+
+        Label totalLabel = new Label("Total: R$ " + totalReservation);
+
+        VBox vbox = new VBox(cashButton, creditCardButton, debitButton, totalLabel);
+        getDialogPane().setContent(vbox);
 
         setResultConverter(dialogButton -> {
             if (dialogButton == finalizeButtonType) {
-                return new Pair<>(comboBox.getValue(), LocalDate.now().toString());
+                RadioButton selectedRadioButton = (RadioButton) group.getSelectedToggle();
+                return new Pair<>(selectedRadioButton.getText(), LocalDate.now().toString());
             }
             return null;
         });
