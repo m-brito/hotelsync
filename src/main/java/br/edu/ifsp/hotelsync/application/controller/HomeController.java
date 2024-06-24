66c3880b6a8da8190d9fcd46e7dc81bfaac55f41
vmacena <macena.v@aluno.ifsp.edu.br>
2024-06-23 22:15:52 -0300

    refactor: home page

--- src/main/java/br/edu/ifsp/hotelsync/application/controller/HomeController.java
+++ src/main/java/br/edu/ifsp/hotelsync/application/controller/HomeController.java
@@ -6,16 +6,27 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-
+import javafx.scene.Cursor;
+import java.awt.*;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class HomeController {
 
     @FXML
     private AnchorPane bakcground;
+
+    @FXML
+    private ImageView githubIcon;
+
+    @FXML
+    private ImageView javaIcon;
 
     @FXML
     private Button btnGuest;
@@ -46,6 +57,7 @@ public class HomeController {
 
    @FXML
     public void initialize() {
+       githubIcon.setCursor(Cursor.HAND);
     }
 
     private final ExitHandler exitHandler =
@@ -53,6 +65,9 @@ public class HomeController {
 
     private final NavigationHandler navHandler =
             new NavigationHandler();
+
+    private final NavigationHandler navigationHandler = new NavigationHandler();
+
 
     @FXML
     void handleExit(ActionEvent event) {
@@ -82,5 +97,10 @@ public class HomeController {
     @FXML
     void handleRoomPage(ActionEvent event) throws IOException {
         navHandler.navigateToRoomManagementPage();
+    }
+
+    @FXML
+    void handleGithubIconClick() throws IOException {
+        navigationHandler.navigateToGithubPage();
     }
 }
