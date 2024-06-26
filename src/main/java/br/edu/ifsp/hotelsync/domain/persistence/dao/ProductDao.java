59f948947f8f034c048b6848ccadf177cb74d8a3
m-brito <britomauricio2003@gmail.com>
2024-06-26 15:21:14 -0300

    feat: adds findAllByName to DAO interface

--- src/main/java/br/edu/ifsp/hotelsync/domain/persistence/dao/ProductDao.java
+++ src/main/java/br/edu/ifsp/hotelsync/domain/persistence/dao/ProductDao.java
@@ -2,10 +2,15 @@ package br.edu.ifsp.hotelsync.domain.persistence.dao;
 
 import br.edu.ifsp.hotelsync.domain.entities.product.Product;
 import br.edu.ifsp.hotelsync.domain.entities.reservation.ConsumedProduct;
+import br.edu.ifsp.hotelsync.domain.entities.reservation.Reservation;
 import br.edu.ifsp.hotelsync.domain.persistence.util.Dao;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ProductDao extends Dao<Long, Product> {
     List<ConsumedProduct> findAllByIdReservation(Long id);
+
+    Map<Long, Product> findAllByName(String name);
+
 }
